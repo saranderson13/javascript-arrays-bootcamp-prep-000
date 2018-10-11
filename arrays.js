6528dac14dcd1b04039fcd6187beea25524a5b94
Sarah Anderson <saranderson13@gmail.com>
2018-10-11 05:02:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -40,7 +40,8 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  array.slice()
+  array.slice(0, array.length - 1);
+  return array;
 }
 
 
