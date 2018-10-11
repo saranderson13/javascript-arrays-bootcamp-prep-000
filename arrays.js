08222116722a195b1130dada2328255fb5c3d0a2
Sarah Anderson <saranderson13@gmail.com>
2018-10-11 04:57:54 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,6 +30,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1);
+  array = array.slice(1);
   return array;
 }
